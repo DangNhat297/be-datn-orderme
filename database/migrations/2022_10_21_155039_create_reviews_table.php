4bb937fff168669189f4bec508ea23cb40b2a2b1
Nguyen Dang Nhat <ndnhat1@gmail.com>
2022-10-21 23:31:59 +0700

    DATN-30 create migration

---
+++ database/migrations/2022_10_21_155039_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('rating')->nullable();
+            $table->text('message');
+            $table->unsignedBigInteger('parent_id');
+            $table->unsignedBigInteger('dish_id');
+            $table->unsignedBigInteger('user_id');
+            $table->tinyInteger('status');
+            $table->integer('like');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
