7774641bee7292b907b24e6b136d851575037245
Nguyen Dang Nhat <ndnhat1@gmail.com>
2022-10-22 00:19:48 +0700

    rebuild base controller for api

--- RULE.md
+++ RULE.md
@@ -3,3 +3,5 @@
 Khai báo các helper function, hằng trong thư mục helper (function.php, const.php)
 
 Thực hiện format code sau mỗi lần code : Alt + Shift + F (vscode cài ext PHP Intelephense)
+
+Thực hiện comment cho các function (kiểu trả về, tham số truyền vào)
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,60 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    /**
+     * sendSuccess
+     *
+     * @param  mixed $data
+     * @param  mixed $code
+     * @param  mixed $message
+     * @return JsonResponse
+     */
+    public function sendSuccess($data, $code = 200, $message = 'Successfully'): JsonResponse
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message
+        ], $code);
+    }
+    
+    /**
+     * createSuccess
+     *
+     * @param  mixed $data
+     * @param  mixed $message
+     * @return JsonResponse
+     */
+    public function createSuccess($data, $message): JsonResponse
+    {
+        return $this->sendSuccess($data, 201, $message);
+    }
+    
+    /**
+     * updateSuccess
+     *
+     * @param  mixed $data
+     * @param  mixed $message
+     * @return JsonResponse
+     */
+    public function updateSuccess($data, $message): JsonResponse
+    {
+        return $this->sendSuccess($data, 201, $message);
+    }
+    
+    /**
+     * deleteSuccess
+     *
+     * @return JsonResponse
+     */
+    public function deleteSuccess(): JsonResponse
+    {
+        return response()->json(null, 204);
+    }
 }
