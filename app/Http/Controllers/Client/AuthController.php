a82c031d0a4eee62497964a04841a29677b8b051
VPNam <vuphuongnam25122002@gmail.com>
2022-11-07 09:42:06 +0700

    fix swagger

--- app/Http/Controllers/Client/AuthController.php
+++ app/Http/Controllers/Client/AuthController.php
@@ -1,55 +1,54 @@
 <?php
 
-    namespace App\Http\Controllers\client;
+namespace App\Http\Controllers\Client;
 
-    use App\Http\Controllers\Controller;
-    use App\Http\Requests\AuthRequest;
-    use App\Models\User;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
 
-    class AuthController extends Controller
+class AuthController extends Controller
+{
+    protected $user;
+
+    public function __construct(User $user)
     {
-        protected $user;
+        $this->user = $user;
+    }
 
-        public function __construct(User $user)
-        {
-            $this->user = $user;
-        }
-
-        public function login(AuthRequest $request)
-        {
-            $user = $this->user->newQuery()->where('phone', $request->phone)->first();
-            if ($user) {
-                $user->update(['name'=>$request->name]);
+    public function login(AuthRequest $request)
+    {
+        $user = $this->user->newQuery()->where('phone', $request->phone)->first();
+        if ($user) {
+            $user->update(['name' => $request->name]);
 //                $user->createToken('API Token')->plainTextToken;
-            } else {
-                $user = $this->user->newQuery()->create($request->all());
+        } else {
+            $user = $this->user->newQuery()->create($request->all());
 //                    ->createToken('API Token')->plainTextToken;
-            }
-
-            return $this->createSuccess($user);
-
         }
 
-        public function profile($id)
-        {
-//            $id=auth()->user()->id;
-            $user=$this->user->newQuery()->findOrFail($id);
-
-            return $this->sendSuccess($user);
-
-        }
-
-        public function update(Request $request,$id)
-        {
-//            $id=auth()->user()->id;
-            $user=$this->user->newQuery()->findOrFail($id);
-            $user->update($request->all());
-
-            return $this->updateSuccess($user);
-
-        }
-
+        return $this->createSuccess($user);
 
     }
+
+    public function update(Request $request, $id)
+    {
+//            $id=auth()->user()->id;
+        $user = $this->user->newQuery()->findOrFail($id);
+        $user->update($request->all());
+
+        return $this->updateSuccess($user);
+
+    }
+
+    public function profile($id)
+    {
+//            $id=auth()->user()->id;
+        $user = $this->user->newQuery()->findOrFail($id);
+
+        return $this->sendSuccess($user);
+
+    }
+
+
+}
