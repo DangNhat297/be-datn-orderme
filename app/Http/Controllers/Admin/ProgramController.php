a70cf9965bd9d49997b43a5ca29d3bc99639eab4
VPNam <vuphuongnam25122002@gmail.com>
2022-12-13 14:11:13 +0700

    [DBE-38] Add swagger api program

--- app/Http/Controllers/Admin/ProgramController.php
+++ app/Http/Controllers/Admin/ProgramController.php
@@ -5,36 +5,91 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ProgramRequest;
 use App\Models\Program;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProgramController extends Controller
 {
     public function __construct(
         protected Program $program
-    ){
+    )
+    {
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *      path="/admin/program",
+     *      operationId="getPrograms",
+     *      tags={"Program"},
+     *      summary="Get list of program",
+     *      description="Returns list of program",
+     *      @OA\Parameter(
+     *          name="keyword",
+     *          description="Search by name flasale",
+     *          required=false,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="status",
+     *          description="program status",
+     *          required=false,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="orderBy",
+     *          description="nhiều trường",
+     *          required=false,
+     *          in="query",
+     *          @OA\Schema(
+     *              type="array",
+     *              @OA\Items(type="string")
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ProgramResponse")
+     *       ),
+     *     )
      */
-    public function index(Request $request)
+    public function index(Request $request): JsonResponse
     {
         $page_size = $request->per_page ?: PAGE_SIZE_DEFAULT;
 
-        $programs = $this->program->newQuery()->paginate($page_size);
+        $programs = $this->program
+            ->newQuery()
+            ->findByTitle($request)
+            ->findByStatus($request)
+            ->paginate($page_size);
 
         return $this->sendSuccess($programs);
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @OA\Post(
+     *      path="/admin/program",
+     *      operationId="createProgram",
+     *      tags={"Program"},
+     *      summary="Create new program",
+     *      description="Returns program data",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/ProgramCreate")
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ProgramResponse")
+     *       ),
+     * )
      */
-    public function store(ProgramRequest $request)
+    public function store(ProgramRequest $request): JsonResponse
     {
         $data = $request->only([
             'title',
@@ -56,24 +111,105 @@ class ProgramController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *      path="/admin/program/{id}",
+     *      operationId="getProgramById",
+     *      tags={"Program"},
+     *      summary="Get program information",
+     *      description="Returns program data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Program id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ProgramResponse")
+     *       ),
+     * )
      */
-    public function show(Program $program)
+    public function show(int $id): JsonResponse
     {
+        $program = $this->program
+            ->newQuery()
+            ->with('dishes')
+            ->findOrFail($id);
         return $this->sendSuccess($program);
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @OA\Delete(
+     *      path="/admin/program/{id}",
+     *      operationId="deleteProgram",
+     *      tags={"Program"},
+     *      summary="Delete existing program",
+     *      description="Deletes a record and returns no content",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Program id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *       @OA\Response(
+     *          response=204,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       )
+     * )
      */
-    public function update(ProgramRequest $request, Program $program)
+    public function destroy($id): JsonResponse
+    {
+        $this->program
+            ->newQuery()
+            ->findOrFail($id)->delete();
+        return $this->deleteSuccess();
+    }
+
+    public function toggleStatus(Program $program): JsonResponse
+    {
+        $status = $program->status == ENABLE ? DISABLE : ENABLE;
+
+        $program->update(['status' => $status]);
+
+        return $this->updateSuccess($program);
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/admin/program/{id}",
+     *      operationId="updateProgram",
+     *      tags={"Program"},
+     *      summary="Update existing program",
+     *      description="Returns updated program data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Program id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/ProgramUpdate")
+     *      ),
+     *      @OA\Response(
+     *          response=202,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ProgramResponse")
+     *       )
+     * )
+     */
+    public function update(ProgramRequest $request, Program $program): JsonResponse
     {
         $data = $request->only([
             'title',
@@ -87,28 +223,6 @@ class ProgramController extends Controller
         $program->update($data);
         $dishIds = $request->dish_ids;
         $program->dishes()->sync($dishIds);
-
-        return $this->updateSuccess($program);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Program $program)
-    {
-        $program->delete();
-
-        return $this->deleteSuccess();
-    }
-
-    public function toggleStatus(Program $program)
-    {
-        $status = $program->status == ENABLE ? DISABLE : ENABLE;
-
-        $program->update(['status' => $status]);
 
         return $this->updateSuccess($program);
     }
---
+++ app/Models/Schemas/Program/ProgramCreate.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models\Schemas\Program;
+/**
+ * @OA\Schema
+ *     schema="ProgramCreate",
+ *     type="object",
+ *     title="ProgramCreate",
+ *     description="Program model"
+ * )
+ */
+class ProgramCreate
+{
+    /**
+     * @OA\Property()
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @OA\Property()
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @OA\Property()
+     *
+     * @var integer
+     */
+    public $discount_percent;
+
+    /**
+     * @OA\Property()
+     *
+     * @var string
+     */
+    public $start_date;
+
+    /**
+     * @OA\Property()
+     *
+     * @var string
+     */
+    public $end_date;
+
+    /**
+     * @OA\Property(
+     *     type="array",
+     *     @OA\Items(type="number")
+     * )
+     *
+     * @var array
+     */
+    public $dish_ids;
+
+    /**
+     * @OA\Property()
+     *
+     * @var string
+     */
+    public $status;
+
+}
