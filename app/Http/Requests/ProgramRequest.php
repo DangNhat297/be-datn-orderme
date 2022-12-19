084ac7714d2dfcc35f689acebffb922f5034d2fb
Nguyễn Đăng Nhật <nhatnd85045@co-well.vn>
2022-12-19 14:03:17 +0700

    remove request program

--- app/Http/Requests/ProgramRequest.php
+++ app/Http/Requests/ProgramRequest.php
@@ -25,28 +25,28 @@ class ProgramRequest extends FormRequest
      */
     public function rules()
     {
-        $rules = [
-            'start_date' => [
-                'required',
-            //    new CheckDateProgram()
-            ],
-            'end_date' => [
-                'required',
-            //    new CheckDateProgram()
-            ],
-            'discount_percent' => 'required_if:type,=,1|integer|min:0|max:100|',
-            'status' => 'required|integer|in:0,1',
-            'title' => 'required',
-            'description' => 'required',
-            'dish_ids' => 'required|array|min:1',
-            'dish_ids.*' => 'exists:dishes,id'
-        ];
+        // $rules = [
+        //     'start_date' => [
+        //         'required',
+        //     //    new CheckDateProgram()
+        //     ],
+        //     'end_date' => [
+        //         'required',
+        //     //    new CheckDateProgram()
+        //     ],
+        //     'discount_percent' => 'required_if:type,=,1|integer|min:0|max:100|',
+        //     'status' => 'required|integer|in:0,1',
+        //     'title' => 'required',
+        //     'description' => 'required',
+        //     'dish_ids' => 'required|array|min:1',
+        //     'dish_ids.*' => 'exists:dishes,id'
+        // ];
 
-        if (request()->method() == 'PUT') {
+        // if (request()->method() == 'PUT') {
             
-            return $rules;
-        }
+        //     return $rules;
+        // }
 
-        return $rules;
+        // return $rules;
     }
 }
